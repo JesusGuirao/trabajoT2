7c059854e61b2321332723ff6b4b15bca80189ef
JesusGuirao <jesguime@gmail.com>
2021-03-03 18:15:39 +0100

    Subida bastante actualizado

--- scriptweb.js
+++ scriptweb.js
@@ -6,3 +6,18 @@ fecha:
 resumen:
 */
 
+/*Contador de veces de la pagina*/
+
+document.getElementById("nVeces").innerHTML = "Has visitado   "+sessionStorage.clickcount + " pagina(s)";
+function clickCounter() {
+    if(typeof(Storage) !== "undefined") {
+      if (sessionStorage.clickcount) {
+        sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
+      } else {
+        sessionStorage.clickcount = 1;
+      }
+      document.getElementById("nVeces").innerHTML = "Has visitado nuestra pagina " + sessionStorage.clickcount + " veces.";
+    } else {
+      document.getElementById("nVeces").innerHTML = "Sorry, Tu navegador no soporta web storage...";
+    }
+  }

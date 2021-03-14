40ccd7707d65b2310d78e31408563f9d72889933
JesusGuirao <jesguime@gmail.com>
2021-03-14 02:16:14 +0100

    Letra de la web cambiada

--- scriptweb.js
+++ scriptweb.js
@@ -8,14 +8,22 @@ resumen:
 
 /*Contador de veces de la pagina*/
 
-actualizar();
-		
+//actualizar();
+
+$('#nVeces').text(
+  'Has hecho click ' + sessionStorage.contador + ' veces en esta sesión.'
+);
+
 function clickCounter() {
-if(sessionStorage.contador) {
-  sessionStorage.contador = Number(sessionStorage.contador) + 1;
+  if (sessionStorage.contador) {
+    sessionStorage.contador = Number(sessionStorage.contador) + 1;
+  } else {
+    sessionStorage.setItem('contador', 1);
+  }
+
+  $('#nVeces').text(
+    'Has hecho click ' + sessionStorage.contador + ' veces en esta sesión.'
+  );
 }
-else{
-  sessionStorage.setItem("contador",1);
-}
-$("#nVeces").text('Has hecho click ' + sessionStorage.contador + ' veces en esta sesión.');
-} 
+
+$('.migas b').text(document.location.href.match(/[^\/]+$/)[0].split('.')[0]);
